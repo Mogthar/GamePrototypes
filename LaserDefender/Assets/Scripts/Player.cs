71fe8717a494d55b46c67f54f2f7ca0b501646cf
Kucman <kucera.j@atlas.cz>
2023-01-05 22:00:04 +0000

    worked on laser defender

--- LaserDefender/Assets/Scripts/Player.cs
+++ LaserDefender/Assets/Scripts/Player.cs
@@ -7,7 +7,19 @@ public class Player : MonoBehaviour
 {
     Vector2 rawInput;
     [SerializeField] float _speed;
+    [SerializeField] float _paddingLeft;
+    [SerializeField] float _paddingRight;
+    [SerializeField] float _paddingBottom;
+    [SerializeField] float _paddingTop;
+    Vector2 _minimumBound;
+    Vector2 _maximumBound;
     // Update is called once per frame
+
+    void Start()
+    {
+        InitBounds();
+    }
+
     void Update()
     {
         Move();
@@ -15,13 +27,23 @@ public class Player : MonoBehaviour
 
     void Move()
     {
-        Vector3 delta = rawInput * _speed * Time.deltaTime;
-        transform.position += delta;
+        Vector2 delta = rawInput * _speed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, _minimumBound.x + _paddingLeft, _maximumBound.x - _paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, _minimumBound.y + _paddingBottom, _maximumBound.y- _paddingTop);
+        transform.position = newPos;
     }
 
     void OnMove(InputValue value)
     {
         rawInput = value.Get<Vector2>();
-        Debug.Log(rawInput);
+        // Debug.Log(rawInput);
+    }
+
+    void InitBounds()
+    {
+        _minimumBound = Camera.main.ViewportToWorldPoint(new Vector2(0,0));
+        _maximumBound = Camera.main.ViewportToWorldPoint(new Vector2(1,1));
+
     }
 }
